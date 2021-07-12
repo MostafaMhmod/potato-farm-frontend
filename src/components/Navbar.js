1db6abce79855c19f7834446c093dbc382180ae0
Moustafa.mahm@gmail.com <moustafa.mahm@gmail.com>
2021-07-13 00:29:53 +0200

    Feat(nav):added nav bar

---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+        <p
+          className="navbar-brand col-sm-3 col-md-2 mr-0 mb-0"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          🧑‍🌾 &nbsp; Potato Farm
+        </p>
+
+        <ul className="navbar-nav px-3">
+          <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
+            <small className="text-secondary">
+              <small id="account">{this.props.account}</small>
+            </small>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
